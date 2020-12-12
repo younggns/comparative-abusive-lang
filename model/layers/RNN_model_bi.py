42fea48a5a76b002dd7c686779a8e2170dac80c2
ryoya ito <110ryoya@gmail.com>
2020-12-12 09:29:06 +0900

    fix imports

--- model/layers/RNN_model_bi.py
+++ model/layers/RNN_model_bi.py
@@ -6,8 +6,6 @@ what    : Single Encoder Model - bidirectional
 data    : twitter
 """
 import tensorflow as tf
-from tensorflow.contrib import rnn
-from tensorflow.contrib.rnn import DropoutWrapper
 
 from tensorflow.core.framework import summary_pb2
 from random import shuffle
